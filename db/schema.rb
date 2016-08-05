56e8d0296c7810feeecf9c6e39bc2ebaf12d5a11
Toño Serna <antonelo@gmail.com>
2016-08-05 23:08:41 +0200

    4.2 Add color field to products and some extra frontend in index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804121456) do
+ActiveRecord::Schema.define(version: 20160805164817) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160804121456) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "color"
   end
 
 end
