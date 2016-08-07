13f962c5b7b2ef0dbec097c7255d2893411c3170
Toño Serna <antonelo@gmail.com>
2016-08-07 19:56:34 +0200

    4.2 Add price and some little changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805164817) do
+ActiveRecord::Schema.define(version: 20160805221018) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160805164817) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.float    "price"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create(:name => "seed-generated product", :description => "I also created this product inside the seed")
+Product.create(:name => "Grey Falcon", :description => "Yes!", image_url: "bike1.jpg")
+Product.create(:name => "White rainbow", :description => "Yes! amazing", image_url: "bike2.jpg")
+Product.create(:name => "black ruby cobra", :description => "My favorite", image_url: "bike6.jpg")
+Product.create(:name => "Best Vintage", :description => "Vintage!", image_url: "bike3.jpg")
+Product.create(:name => "Best Vintage", :description => "Vintage!", image_url: "https://s-media-cache-ak0.pinimg.com/736x/5c/d2/da/5cd2da34ff92d6f2875ebb95de75cefc.jpg")
