c6ef2267dc5bff7e2e3070c59202a7be9c403127
Toño Serna <antonelo@gmail.com>
2016-08-24 02:33:30 +0200

    5.7 Add comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def rest_of_comments
+    comments.rating_desc.drop(1)
+  end
 end
