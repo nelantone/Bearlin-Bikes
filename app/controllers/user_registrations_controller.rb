26b7b5fbcce43423d8eec96a062f3aa21453c775
Toño Serna <antonelo@gmail.com>
2016-08-30 11:10:43 +0200

    5.11 Add automated email on user sign up

---
+++ app/controllers/user_registrations_controller.rb
@@ -0,0 +1,1 @@
+user_registrations_controller.rb
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   # get 'static_pages/index'
   root 'products#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'},
+    :controllers => { :registrations => "user_registrations" }
 end
