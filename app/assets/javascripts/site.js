88dd3b2fb69db17b92e81e84b8ff6377a2cb01f1
Toño Serna <antonelo@gmail.com>
2016-09-11 02:51:40 +0200

    6.9 Add jQuery Plugin: elevateZoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,5 @@
 $(document).on('turbolinks:load', function(){
+  $('.img-zoom').elevateZoom();
   $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
   $('.rated').raty({ path: '/assets',
     readOnly: true,
