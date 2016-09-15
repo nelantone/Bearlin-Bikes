b72abb9efb6b121dc54e3f7ee645e797c2da2a0d
Toño Serna <antonelo@gmail.com>
2016-09-15 18:32:59 +0200

    6.12 Redis autocomplete *Pending test & maybe fix

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
   # get 'static_pages/index'
   root 'products#landing_page'
+  match '/search_suggestions', to: 'products#index', via: :get
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'},
     :controllers => { :registrations => "user_registrations" }
