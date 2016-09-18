0aa7ce43e3749eb0974049de251a8755e47b94c8
Antonio Serna Gutierrez <antonelo@gmail.com>
2016-09-18 23:26:05 +0200

    Revert "6.6 Add Payment front-end and rest of stripe test setup payment"

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   default from: "from@example.com"
 
   def contact_form(email, name, message)
-    @message = message
+  @message = message
     mail(:from => email,
         :to => 'antonelo@gmail.com',
         :subject => "New Bearlin-Bikes contact form message from #{name}")
@@ -13,12 +13,4 @@ class UserMailer < ApplicationMailer
     mail( :to => user.email,
           :subject => "Welcome to #{@appname}! thanks for signing up")
   end
-
-  # def create(email, product, charge)
-  #   @appname = "Bearlin Bikes"
-  #   mail( :to => email,
-  #         :subject => "Thank you for buying in #{@appname}! Here is your order") do |format|
-  #     format.html { render 'mail_order' }
-  #   end
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  post 'payments/create'
-
   get '/products/:id', to: 'products#show'
 
   post 'static_pages/thank_you'
